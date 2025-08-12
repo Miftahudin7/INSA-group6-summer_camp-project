f2010620ad90133233d56a267d80b0d853733463
Abdurehman Kero <keroabdurehman@gmail.com>
2025-08-12 17:53:58 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() { 
+  return (
+    <div>
+      <h1 className=''>Hello world</h1>
+      <h1>hello</h1>
+      <br />  
+    </div>
+  );
+}
+
+export default App
